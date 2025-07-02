bcd8336b4b180e382adab6de4158f73d490d717d
nelifzyh <nelifauziyah07@gmail.com>
2025-07-02 14:31:51 +0700

    update preprocess sesuai train

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, jsonify
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.efficientnet import preprocess_input
 import numpy as np
-import io
 
 app = Flask(__name__)
 
@@ -19,14 +19,12 @@ def predict():
         return jsonify({'error': 'No file uploaded'}), 400
 
     file = request.files['file']
-    
-    # Convert file ke BytesIO stream
-    img_bytes = file.read()
-    img = image.load_img(io.BytesIO(img_bytes), target_size=(224, 224))
-
+    img = image.load_img(file, target_size=(224, 224))
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
-    img_array = img_array / 255.0
+
+    # PREPROCESS SESUAI TRAINING
+    img_array = preprocess_input(img_array)
 
     preds = model.predict(img_array)
     pred_index = np.argmax(preds)
